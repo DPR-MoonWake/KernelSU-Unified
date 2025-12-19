d828fb7da3bcab959a125997070201ae68873259
RainyXeon <rainyxeon@gmail.com>
2025-12-19 13:01:20 +0700

    add: kernelsu next specific function for kernelsu next

--- kernel/supercalls.h
+++ kernel/supercalls.h
@@ -88,6 +88,14 @@ struct ksu_nuke_ext4_sysfs_cmd {
 	__aligned_u64 arg; // Input: mnt pointer
 };
 
+struct ksu_get_hook_mode_cmd {
+	char mode[32];
+};
+
+struct ksu_get_version_tag_cmd {
+	char tag[32];
+};
+
 #define KSU_MARK_GET 1
 #define KSU_MARK_MARK 2
 #define KSU_MARK_UNMARK 3
@@ -122,6 +130,9 @@ struct ksu_add_try_umount_cmd {
 #define KSU_IOCTL_MANAGE_MARK _IOC(_IOC_READ | _IOC_WRITE, 'K', 16, 0)
 #define KSU_IOCTL_NUKE_EXT4_SYSFS _IOC(_IOC_WRITE, 'K', 17, 0)
 #define KSU_IOCTL_ADD_TRY_UMOUNT _IOC(_IOC_WRITE, 'K', 18, 0)
+#define KSU_IOCTL_GET_HOOK_MODE _IOC(_IOC_READ, 'K', 19, 0)
+#define KSU_IOCTL_GET_VERSION_TAG _IOC(_IOC_READ, 'K', 20, 0)
+
 
 // IOCTL handler types
 typedef int (*ksu_ioctl_handler_t)(void __user *arg);
