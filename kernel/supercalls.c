e905f6b1b433a396cd8cd7c4717d9e3cf2b0c8e4
RainyXeon <rainyxeon@gmail.com>
2025-12-19 13:01:20 +0700

    add: kernelsu next specific function for kernelsu next

--- kernel/supercalls.c
+++ kernel/supercalls.c
@@ -96,6 +96,45 @@ static int do_get_info(void __user *arg)
 	return 0;
 }
 
+// GET_VERSION_TAG - Get version tag (KernelSU-Next Compatibility)
+static int do_get_version_tag(void __user *arg)
+{
+	struct ksu_get_version_tag_cmd cmd;
+
+	strscpy(cmd.tag, KERNEL_SU_NEXT_VERSION_TAG, sizeof(cmd.tag));
+
+	if (copy_to_user(arg, &cmd, sizeof(cmd))) {
+		pr_err("get_version_tag: copy_to_user failed\n");
+		return -EFAULT;
+	}
+
+	return 0;
+}
+
+// GET_HOOK_MODE - Get hook mode (KernelSU-Next Compatibility)
+static int do_get_hook_mode(void __user *arg)
+{
+    struct ksu_get_hook_mode_cmd cmd;
+    const char *type = "Manual";
+
+#if defined(CONFIG_KSU_SUSFS)
+    type = "Inline";
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
+    strscpy(cmd.mode, type, sizeof(cmd.mode));
+#else
+    strlcpy(cmd.mode, type, sizeof(cmd.mode));
+#endif
+
+    if (copy_to_user(arg, &cmd, sizeof(cmd))) {
+        pr_err("get_hook_type: copy_to_user failed\n");
+        return -EFAULT;
+    }
+
+    return 0;
+}
+
 static int do_report_event(void __user *arg)
 {
 	struct ksu_report_event_cmd cmd;
@@ -596,6 +635,9 @@ static const struct ksu_ioctl_cmd_map ksu_ioctl_handlers[] = {
 		  manager_or_root),
 	KSU_IOCTL(ADD_TRY_UMOUNT, "ADD_TRY_UMOUNT", add_try_umount,
 		  manager_or_root),
+	// KernelSU-Next Compatibility
+	KSU_IOCTL(GET_VERSION_TAG, "GET_VERSION_TAG", do_get_version_tag, manager_or_root),
+	KSU_IOCTL(GET_HOOK_MODE, "GET_HOOK_MODE", do_get_hook_mode, manager_or_root),
 
 	// Sentinel
 	{ .cmd = 0, .name = NULL, .handler = NULL, .perm_check = NULL }
